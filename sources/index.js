52814fb290ac97fbb44f2374b9096201e84364cf
dominikawalska <dominikawalska@gmail.com>
2021-11-14 21:44:37 +0100

    Added unit conversion
    Also styled buttons and cleaned JS

--- sources/index.js
+++ sources/index.js
@@ -24,7 +24,6 @@ function currentDate(date) {
 let now = new Date();
 document.querySelector("#date").innerHTML = currentDate(now);
 
-// Get temperature, wind, humidity and description
 function displayWeatherCondition(response) {
   let cityElement = document.querySelector("#main-city");
   let temperatureElement = document.querySelector(".main-weather-temperature");
@@ -33,6 +32,8 @@ function displayWeatherCondition(response) {
   let descriptionElement = document.querySelector("#description");
   let iconElement = document.querySelector("#icon");
   let icon = response.data.weather[0].icon;
+
+  celsiusTemperatue = response.data.main.temp;
 
   cityElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -46,16 +47,12 @@ function displayWeatherCondition(response) {
   iconElement.setAttribute("alt", response.data.weather[0].main);
 }
 
-// default city
 function search(city) {
   let units = "metric";
   let apiKey = "b0bc51fbf47aded8fd93326b74c97af1";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-
   axios.get(apiUrl).then(displayWeatherCondition);
 }
-
-// Get city
 
 function showCity(event) {
   event.preventDefault();
@@ -63,19 +60,12 @@ function showCity(event) {
   search(city);
 }
 
-let form = document.querySelector("#city-form");
-form.addEventListener("submit", showCity);
-
-//Get my location
-
 function handleCurrentPosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   let units = "metric";
   let apiKey = "b0bc51fbf47aded8fd93326b74c97af1";
-
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
-
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
@@ -84,26 +74,34 @@ function getPosition(event) {
   navigator.geolocation.getCurrentPosition(handleCurrentPosition);
 }
 
-let locationButton = document.querySelector(".my-location-button");
-locationButton.addEventListener("click", getPosition);
-
-// Get Celcius and Fahrenheit
-
 function changeToCelcius(event) {
   event.preventDefault();
   let celsiusElement = document.querySelector(".main-weather-temperature");
-  celsiusElement.innerHTML = "18";
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  celsiusElement.innerHTML = Math.round(celsiusTemperatue);
 }
-
-let celsiusUnit = document.querySelector("#celsius");
-celsiusUnit.addEventListener("click", changeToCelcius);
 
 function changeToFahrenheit(event) {
   event.preventDefault();
   let fahrenheitElement = document.querySelector(".main-weather-temperature");
-  fahrenheitElement.innerHTML = "64";
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  fahrenheitElement.innerHTML = Math.round((9 / 5) * celsiusTemperatue + 32);
 }
-let fahrenheitUnit = document.querySelector("#fahrenheit");
-fahrenheitUnit.addEventListener("click", changeToFahrenheit);
+
+let celsiusTemperatue = null;
+
+let form = document.querySelector("#city-form");
+form.addEventListener("submit", showCity);
+
+let locationButton = document.querySelector(".my-location-button");
+locationButton.addEventListener("click", getPosition);
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", changeToCelcius);
+
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", changeToFahrenheit);
 
 search("Warszawa");
